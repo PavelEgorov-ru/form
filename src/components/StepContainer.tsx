566e02c8ba6d461ecc9f5b81d7ab70c94981601e
Павел Егоров <pavel@192.168.0.8>
2022-10-16 14:25:27 +0300

    удаление оставшейся ненужной функциональности

--- src/components/StepContainer.tsx
+++ src/components/StepContainer.tsx
@@ -7,76 +7,28 @@ import {
   Button,
   Typography,
   Box,
+  dividerClasses,
 } from "@mui/material";
 
 const steps = ["Логин и пароль", "Где проживаете", "Номер телефона"];
 
 const StepContainer = () => {
   const [activeStep, setActiveStep] = useState(0);
-
-  const isStepOptional = (step: number) => {
-    return step === 1;
-  };
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
   return (
     <Box sx={{ width: "100%" }}>
       <Stepper activeStep={activeStep}>
         {steps.map((label, index) => {
           const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
           return (
             <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
+              <StepLabel>{label}</StepLabel>
             </Step>
           );
         })}
       </Stepper>
-      {activeStep === steps.length ? (
-        <>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Box sx={{ flex: "1 1 auto" }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </>
-      ) : (
-        <>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: "1 1 auto" }} />
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? "Finish" : "Next"}
-            </Button>
-          </Box>
-        </>
-      )}
+      {activeStep === 0 && <div>шаг логин и пароль</div>}
+      {activeStep === 1 && <div>шаг где проживаете</div>}
+      {activeStep === 2 && <div>шаг номер телефона</div>}
     </Box>
   );
 };
