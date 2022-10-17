2bbff8e5b8cb9392d4854f091191edfb737b61a1
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-17 22:41:33 +0300

    инпуты для экрана телефона

--- src/components/StepsContainer/Steps/PhoneStep.tsx
+++ src/components/StepsContainer/Steps/PhoneStep.tsx
@@ -1,10 +1,36 @@
-import { Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
+import { formActions } from "../../../services/reducers";
+import { Button, TextField } from "@mui/material";
 import LayoutStep from "./LayoutStep";
 
 const PhoneStep = () => {
   return (
     <LayoutStep>
-      <Typography> Шаг с вводом телефона </Typography>
+      <TextField
+        name="phone"
+        placeholder="Телефон *"
+        fullWidth
+        required
+        // onChange={(e) => changeInput(e)}
+        // value={form.login}
+      ></TextField>
+      <Button
+        key="next"
+        variant="contained"
+        // onClick={next}
+        // disabled={isActiveButton}
+      >
+        Получить код
+      </Button>
+      <TextField
+        name="code"
+        placeholder="Введите полученый код *"
+        fullWidth
+        required
+        // onChange={(e) => changeInput(e)}
+        // value={form.email}
+      ></TextField>
     </LayoutStep>
   );
 };
