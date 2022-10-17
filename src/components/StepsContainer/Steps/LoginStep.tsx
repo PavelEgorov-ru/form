4577339bdc97463f10656b522ab95b257457ca33
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-17 15:50:52 +0300

    переименование функции записи состояния инпута

--- src/components/StepsContainer/Steps/LoginStep.tsx
+++ src/components/StepsContainer/Steps/LoginStep.tsx
@@ -9,12 +9,12 @@ const LoginStep = () => {
     name: "",
   });
 
-  const test = (
+  const changeInput = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const id = e.target.id;
     const value = e.target.value;
-    console.log(e.target.required);
+    console.log(e.target.name);
 
     setInputs((prevState) => ({
       ...prevState,
@@ -23,24 +23,29 @@ const LoginStep = () => {
   };
 
   const isReq = (obj: any) => {
+    console.log("вызов функции");
     for (const key in obj) {
       if (obj[key] === "") return false;
     }
     return true;
   };
 
-  console.log("isReq", isReq(inputs));
+  // console.log("isReq", isReq(inputs));
 
   return (
     <LayoutStep>
       <Typography> шаг с вводом логина </Typography>
       <TextField
-        id="email"
+        name="email"
         fullWidth
         required
-        onChange={(e) => test(e)}
+        onChange={(e) => changeInput(e)}
       ></TextField>
-      <TextField id="password" fullWidth onChange={(e) => test(e)}></TextField>
+      <TextField
+        name="password"
+        fullWidth
+        onChange={(e) => changeInput(e)}
+      ></TextField>
     </LayoutStep>
   );
 };
