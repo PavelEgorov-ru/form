a2ba5bfbef6cda3ce215e42f528299d79c5db42d
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-17 22:31:00 +0300

    инпуты для второго экрана

--- src/components/StepsContainer/Steps/AddressStep.tsx
+++ src/components/StepsContainer/Steps/AddressStep.tsx
@@ -5,14 +5,45 @@ import { TextField } from "@mui/material";
 import LayoutStep from "./LayoutStep";
 
 const AddressStep = () => {
+  const dispatch = useAppDispatch();
+  const { form } = useAppSelector((store) => store.formState);
+
+  const [inputsRequired, setInputsRequired] = useState({
+    country: form.country,
+    city: form.city,
+  });
+
   return (
     <LayoutStep>
       <TextField
-        name="login"
-        placeholder="Логин"
+        name="country"
+        placeholder="Страна *"
         fullWidth
-        // onChange={(e) => changeInput(e)}
-        // value={form.login}
+        required
+        onChange={(e) => changeInput(e)}
+        value={form.country}
+      ></TextField>
+      <TextField
+        name="city"
+        placeholder="Город *"
+        fullWidth
+        required
+        onChange={(e) => changeInput(e)}
+        value={form.city}
+      ></TextField>
+      <TextField
+        name="street"
+        placeholder="Улица"
+        fullWidth
+        onChange={(e) => changeInput(e)}
+        value={form.street}
+      ></TextField>
+      <TextField
+        name="house"
+        placeholder="Дом"
+        fullWidth
+        onChange={(e) => changeInput(e)}
+        value={form.house}
       ></TextField>
     </LayoutStep>
   );
