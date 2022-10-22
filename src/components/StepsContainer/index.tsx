dd1b25fc1f8b7cfba001154a7d7009b95099a2a0
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-22 16:08:07 +0300

    заготовка стейта формы и функции

--- src/components/StepsContainer/index.tsx
+++ src/components/StepsContainer/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { useAppSelector } from "../../hooks";
 
 import { Box } from "@mui/material";
@@ -8,13 +8,40 @@ import PhoneStep from "./Steps/PhoneStep";
 import LoginStep from "./Steps/LoginStep";
 
 const StepContainer = () => {
-  const { stepCount } = useAppSelector((store) => store.formState);
+  // const { stepCount } = useAppSelector((store) => store.formState);
+  const [stateForm, setStateForm] = useState({
+    stepCount: 0,
+    login: "",
+    email: "",
+    password: "",
+    passwordRepet: "",
+    country: "",
+    city: "",
+    street: "",
+    house: "",
+    phone: "",
+    code: "",
+  });
+
+  const changeInputForm = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const { name, value } = e.target;
+      setStateForm((prevState) => {
+        return {
+          ...prevState,
+          [name]: value,
+        };
+      });
+    },
+    []
+  );
+
   return (
     <Box component="form" sx={{ width: "100%" }}>
-      <StepsHeader stepCount={stepCount} />
-      {stepCount === 0 && <LoginStep />}
-      {stepCount === 1 && <AddressStep />}
-      {stepCount === 2 && <PhoneStep />}
+      <StepsHeader stepCount={stateForm.stepCount} />
+      {stateForm.stepCount === 0 && <LoginStep />}
+      {stateForm.stepCount === 1 && <AddressStep />}
+      {stateForm.stepCount === 2 && <PhoneStep />}
     </Box>
   );
 };
