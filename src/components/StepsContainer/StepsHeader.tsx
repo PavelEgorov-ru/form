0e18f2909667e95554e81f76773e89cfe147ba3a
PavelEgorov-ru <pkotofffff@yandex.ru>
2022-10-22 13:11:47 +0300

    готово

--- src/components/StepsContainer/StepsHeader.tsx
+++ src/components/StepsContainer/StepsHeader.tsx
@@ -1,5 +1,23 @@
-const StepsHeader = () => {
-  return <div></div>;
-};
+import React, { FC } from "react";
+import type { TStepHeader } from "../../types";
+
+import { Stepper, Step, StepLabel } from "@mui/material";
+
+const steps = ["Логин и пароль", "Где проживаете", "Номер телефона"];
+
+const StepsHeader: FC<TStepHeader> = React.memo(({ stepCount }) => {
+  return (
+    <Stepper activeStep={stepCount}>
+      {steps.map((label, index) => {
+        const stepProps: { completed?: boolean } = {};
+        return (
+          <Step key={label} {...stepProps}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        );
+      })}
+    </Stepper>
+  );
+});
 
 export default StepsHeader;
--- src/components/StepsContainer/index.tsx
+++ src/components/StepsContainer/index.tsx
@@ -1,27 +1,17 @@
 import React from "react";
 import { useAppSelector } from "../../hooks";
 
-import { Stepper, Step, StepLabel, Box } from "@mui/material";
+import { Box } from "@mui/material";
+import StepsHeader from "./StepsHeader";
 import AddressStep from "./Steps/AddressStep";
 import PhoneStep from "./Steps/PhoneStep";
 import LoginStep from "./Steps/LoginStep";
-
-const steps = ["Логин и пароль", "Где проживаете", "Номер телефона"];
 
 const StepContainer = React.memo(() => {
   const { stepCount } = useAppSelector((store) => store.formState);
   return (
     <Box component="form" sx={{ width: "100%" }}>
-      <Stepper activeStep={stepCount}>
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
+      <StepsHeader stepCount={stepCount} />
       {stepCount === 0 && <LoginStep />}
       {stepCount === 1 && <AddressStep />}
       {stepCount === 2 && <PhoneStep />}
